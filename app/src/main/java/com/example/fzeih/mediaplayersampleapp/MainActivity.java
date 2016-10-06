f6bc3abb11778cdaa3451ecc6d815c9ea6af6b6d
Fabi <fzeiher@gmail.com>
2016-10-06 12:21:15 +0200

    feat: add OnCompletionListener

--- app/src/main/java/com/example/fzeih/mediaplayersampleapp/MainActivity.java
+++ app/src/main/java/com/example/fzeih/mediaplayersampleapp/MainActivity.java
@@ -37,5 +37,12 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+            Toast.makeText(MainActivity.this, "I'm done", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
